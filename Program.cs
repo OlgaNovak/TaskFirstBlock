477d89d2056db89254b8bac4722cc329c86a4800
Olga <olgamelehina@yandex.ru>
2022-10-05 11:46:25 +0300

    Add Solution Programm

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Итоговая проверочная работа
+
+Console.Clear();
+Console.WriteLine("Заполните строкy через запятую:");
+string A=Console.ReadLine();
+string []rows=A.Split(",");
+int N=3;
+Console.WriteLine($"Cтроки массива, длина которых меньше или равна трем символам:");
+for(int i=0; i<rows.Length;i++){
+    if (rows[i].Length<=N)
+    Console.Write(rows[i]+" ,");
+}
+Console.ReadLine();
